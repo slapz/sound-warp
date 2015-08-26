a19b34d28eabe8f36e0c57b50893457884b10d9c
Dmitriy Kholodilin <dkholodilin@live.com>
2015-08-26 23:29:04 +0300

    Fix node_modules directory check

--- application/build.sh
+++ application/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-[[ -d "./node_modules" ]] && npm install >> ./logs/npm.log;
+[[ ! -d "./node_modules" ]] && npm install >> ./logs/npm.log;
 ./node_modules/.bin/gulp >> ./logs/gulp.log;
