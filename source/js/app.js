d9c2cf1254e9349dedb79ffb6a4014b0e4471702
Dmitriy Kholodilin <dkholodilin@live.com>
2015-09-21 13:58:20 +0300

    Add song display

--- source/js/app.js
+++ source/js/app.js
@@ -8,6 +8,8 @@
     win.app = app;
     win.remote = require('remote');
 
+    setVar('display.main-row', '&nbsp;');
+
     app.api = require(__dirname + '/js/app/api')();
     app.player = require(__dirname + '/js/app/player')();
 
@@ -15,18 +17,15 @@
 
     $('[data-trigger="connect.connect"]').on('click', function($event) {
       $event.preventDefault();
+      setVar('display.main-row', 'Connecting SoundCloud...');
       app.api.connect(function(err) {
         if (err) throw err;
 
         connectOverlay.remove();
 
+        setVar('display.main-row', 'Connected to SoundCloud');
         initProfile(app);
       });
-    });
-
-    $('[data-trigger="connect.skip"]').on('click', function($event) {
-      $event.preventDefault();
-      connectOverlay.remove();
     });
 
 })(window);
--- source/js/app/profile.js
+++ source/js/app/profile.js
@@ -2,9 +2,16 @@
 
 module.exports = function(app) {
 
+  setVar('display.sub-row', 'Loading profile information...');
+
   app.api.SC.get('/me', function(profile) {
     setVar('profile.username', profile.full_name || profile.username);
     setSrc('profile.avatar_url', profile.avatar_url);
+
+    setVar('display.sub-row', 'Profile information loaded');
+
+    setVar('display.main-row', '&nbsp;');
+    setVar('display.sub-row', '&nbsp;');
   });
 
 };
